75229464f3ee1cb18ea8983d8e3a75023dd91cce
Arne DB <debelserarne@hotmail.com>
2018-03-20 16:24:10 +0100

    Added no email support
    I had an issue where the script wouldn't send any emails, so I made a log of every file scanned.
    
    Also at the bottom I print the lines which should normally be sent by email, in this case the infected files.

--- phpMalCodeScanner.php
+++ phpMalCodeScanner.php
@@ -9,17 +9,11 @@ Author URI: http://www.mikestowe.com
 Credits: Based on the idea of Er. Rochak Chauhan (http://www.rochakchauhan.com/), rewritten for use with a cron job
 License: GPL-2
 */
-
-
 // Set to your email:
-define('SEND_EMAIL_ALERTS_TO','youremail@example.com');
-
-
+define('SEND_EMAIL_ALERTS_TO','debelserarne@hotmail.com');
 ############################################ START CLASS
-
-
+echo 'Setting up<br>';
 class phpMalCodeScan {
-
 	public $infected_files = array();
 	private $scanned_files = array();
 	
@@ -33,17 +27,20 @@ class phpMalCodeScan {
 	function scan($dir) {
 		$this->scanned_files[] = $dir;
 		$files = scandir($dir);
-		
+		echo '<br><br>';	
+		echo 'Directory: ';
+		echo ($dir) . '<br>';		
 		if(!is_array($files)) {
 			throw new Exception('Unable to scan directory ' . $dir . '.  Please make sure proper permissions have been set.');
 		}
-		
 		foreach($files as $file) {
 			if(is_file($dir.'/'.$file) && !in_array($dir.'/'.$file,$this->scanned_files)) {
 				$this->check(file_get_contents($dir.'/'.$file),$dir.'/'.$file);
 			} elseif(is_dir($dir.'/'.$file) && substr($file,0,1) != '.') {
 				$this->scan($dir.'/'.$file);
 			}
+			echo 'File: ';
+			echo ($file) . '<br>';	
 		}
 	}
 	
@@ -52,30 +49,26 @@ class phpMalCodeScan {
 		$this->scanned_files[] = $file;
 		if(preg_match('/(?<![a-z0-9_])eval\((base64|eval|\$_|\$\$|\$[A-Za-z_0-9\{]*(\(|\{|\[))/i',$contents)) {
 			$this->infected_files[] = $file;
+			echo '<br>';
+			echo '<b>We scanned this file: </b><br>';
 		}
 	}
-
-
 	function sendalert() {
 		if(count($this->infected_files) != 0) {
-			$message = "== MALICIOUS CODE FOUND == \n\n";
-			$message .= "The following files appear to be infected: \n";
+			$message = "== MALICIOUS CODE FOUND == <br>";
+			$message .= "The following files appear to be infected: <br>";
 			foreach($this->infected_files as $inf) {
-				$message .= "  -  $inf \n";
+				$message .= "  $inf \n";
 			}
 			mail(SEND_EMAIL_ALERTS_TO,'Malicious Code Found!',$message,'FROM:');
+			echo '<br><br><b>Email sent!<br>';
+			echo '<br><br><br>';
+			echo $message;
+			echo '</b>';
 		}
 	}
-
-
 }
-
-
 ############################################ INITIATE CLASS
-
 ini_set('memory_limit', '-1'); ## Avoid memory errors (i.e in foreachloop)
-
 new phpMalCodeScan;
-
-
 ?>
